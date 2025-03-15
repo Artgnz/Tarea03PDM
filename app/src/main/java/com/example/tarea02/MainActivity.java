6815151f0b83a09c7d74f910956a7e2831fb7250
Arturo González Peñaloza <arturognz@ciencias.unam.mx>
2025-03-14 21:02:09 -0600

    Agrega vistas a main

--- app/src/main/java/com/example/tarea02/MainActivity.java
+++ app/src/main/java/com/example/tarea02/MainActivity.java
@@ -2,23 +2,16 @@ package com.example.tarea02;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // actividad principal de registro
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // actividad de confirmación
+        //setContentView(R.layout.activity_confirmacion);
     }
 }
